fc663c13960195edae1240348b6578bb499db72a
unknown <herrera.fernando1209@gmail.com>
2024-11-24 00:42:57 -0300

    lazyload

---
+++ react-app/src/01-lazyload/layout/LazyLayout.tsx
@@ -0,0 +1,9 @@
+const LazyLayout = () => {
+  return (
+    <div>
+      <h1>LazyLayout Page</h1>
+      {/* rutas hijas */}
+    </div>
+  );
+};
+export default LazyLayout;
---
+++ react-app/src/01-lazyload/pages/NoLazy.tsx
@@ -0,0 +1,8 @@
+const NoLazy = () => {
+  return (
+    <div>
+      <h1>NoLazy Component</h1>
+    </div>
+  );
+};
+export default NoLazy;
