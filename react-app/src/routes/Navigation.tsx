794df96b899f3d1a7defebb7fa888a94eaca8594
unknown <herrera.fernando1209@gmail.com>
2024-11-27 00:42:16 -0300

    forms components field add

--- react-app/src/routes/Navigation.tsx
+++ react-app/src/routes/Navigation.tsx
@@ -5,13 +5,14 @@ import {
   Route,
   Routes,
 } from "react-router-dom";
-
 import logo from "../assets/react.svg";
-import { RegisterPage } from "../03-forms/pages/RegisterPage";
-import { FormikBasicPage } from "../03-forms/pages/FormikBasicPage";
-import { FormikYupPage } from "../03-forms/pages/FormikYupPage";
-import { FormikComponents } from "../03-forms/pages/FormikComponents";
-import { FormikAbstraction } from "../03-forms/pages/FormikAbstraction";
+import {
+  FormikAbstraction,
+  FormikBasicPage,
+  FormikComponents,
+  FormikYupPage,
+  RegisterPage,
+} from "../03-forms/pages";
 
 export const Navigation = () => {
   return (
